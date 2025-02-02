31157d017a314820cc7ae2965db2cbafb7840189
sashastahiev <vebium@mail.ru>
2025-02-02 22:31:35 +0300

    Completed hw-6

--- playGame2.js
+++ playGame2.js
@@ -1,28 +1,39 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function startGame2() {
     const operators = ['+', '-', '*', '/'];
-    const operator = operators[getRandomArbitrary(0, operators.length - 1)];
-    const num1 = getRandomArbitrary(1, 20);
-    const num2 = getRandomArbitrary(1, 20);
+    const operator = operators[getRandomInt(0, operators.length - 1)];
+    const num1 = getRandomInt(1, 20);
+    const num2 = getRandomInt(1, 20);
     let task;
     let answer;
     switch(operator) {
         case '+':
+            {
             task = `${num1} + ${num2}`;
             answer = num1 + num2;
             break;
+            }
         case '-':
+            {
             task = `${num1} - ${num2}`;
             answer = num1 - num2;
             break;
+            }
         case '*':
+            {
             task = `${num1} * ${num2}`;
             answer = num1 * num2;
             break;
+            }
         case '/':
+            {
             answer = num1; 
             num1 = answer * num2;
             task = `${num1} / ${num2}`;
             break;
+            }
     }
     const userAnswer = prompt(`Решите задачу: ${task}`);
     if (parseFloat(userAnswer) === answer) {
